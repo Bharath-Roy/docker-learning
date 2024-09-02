3228c1f30659a6c7ef8605beadef3d18e547316a
Bharath janaki <janakibharath19@gmail.com>
2024-09-02 17:35:49 +0530

    Update script.py

--- script.py
+++ script.py
@@ -23,7 +23,7 @@ for i in range(1, 11):
 url = 'https://jsonplaceholder.typicode.com/users/2'
 
 print("Script is OK")
-print("first time running")
+print("second time running")
 response = requests.get(url)
 print(response.status_code)
 
